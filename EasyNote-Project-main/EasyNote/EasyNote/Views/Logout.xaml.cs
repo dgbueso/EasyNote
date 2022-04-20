ebafba7048882e9543ee1e0f21b2048319464869
danie <danie@DANIEL-GB>
2022-04-19 23:01:57 -0600

    Add project files.

---
+++ EasyNote-Project-main/EasyNote/EasyNote/Views/Logout.xaml.cs
@@ -0,0 +1,30 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using Xamarin.Essentials;
+using Xamarin.Forms;
+using Xamarin.Forms.Xaml;
+
+namespace EasyNote.Views
+{
+    [XamlCompilation(XamlCompilationOptions.Compile)]
+    public partial class Logout : ContentPage
+    {
+        public Logout()
+        {
+            InitializeComponent();
+
+        }
+
+        private async void Button_Clicked(object sender, EventArgs e)
+        {
+            Preferences.Remove("users");
+            Preferences.Remove("pass");
+            Preferences.Remove("Remember");
+            Preferences.Set("Remember", true);
+            await Navigation.PushModalAsync(new LoginPage());
+        }
+    }
+}
